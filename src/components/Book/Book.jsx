7e6030b42f3cec9a36fd1c69b94722e6a5d3f4cd
Premforreal <93695810+Premforreal@users.noreply.github.com>
2022-10-06 11:33:33 +0530

    books page styling done

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {Button} from '@mui/material'; 
+import './book.css';
 
-function Book() {
+function Book(props) {
+  const {_id,name,author,description,image,price} = props.book;
+
   return (
-    <div>Book</div>
+    <div className='card'>
+      <img src={image} alt={name} />
+      <div className='card-details'>
+        <h2>{name}</h2>
+        <article>by {author}</article>
+        <p>{price}ratings</p>
+        <p>{description}</p>
+          <Button>Update</Button>
+          <Button>Delete</Button>
+      </div>
+    </div>
   )
 }
 
--- src/components/Book/Books.jsx
+++ src/components/Book/Books.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+import Book from './Book';
+
+const URL = "http://localhost:5000/books";
+const fetchHandler = async()=>{
+return await axios.get(URL).then((res)=>res.data)
+}
 
 function Books() {
+  const [books, setBooks] = useState();
+  
+  useEffect(() => {
+      fetchHandler().then(data=>setBooks(data.books));
+  },[]);
+  
+  
   return (
-    <div>Books</div>
+    <div className='Books'>
+        {books && books.map((book,i)=>(
+              <Book key={i} book={book}/>
+        ))}
+    </div>
   )
 }
 
-export default Books
+export default Books;
