a9362e519dc82354eb1997b2e596be7c208fec12
Premforreal <93695810+Premforreal@users.noreply.github.com>
2022-10-09 19:22:56 +0530

    final commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,11 +4,19 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TerminalIcon from '@mui/icons-material/Terminal';
 import './Book/book.css';
 import EmailIcon from '@mui/icons-material/Email';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 function About() {
   return (<div className='About'>
           <h1>About</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique ab fugit voluptatem molestiae, est consequatur delectus, dolor vitae magnam tenetur reiciendis velit error enim nobis ut, rem totam molestias quasi! Dicta omnis aliquid facere id quis culpa dolorem sunt ut reprehenderit, inventore est! Veniam quas consequuntur iure commodi esse expedita.</p>
+          <p>This is a 
+               <Link href="https://www.goodreads.com/" target="_blank" rel="noopener noreferrer"> good reads </Link> 
+                clone with CRUD functionality made using MERN stack. 
+                Bestreads is my attempt to implement my beginner skills on node and react.
+                Still, many things need to be covered like pagination, multiple users 
+                and authentication etc. The project will be upgraded with all the useful features in future. <br/>
+                Please mail me your suggestions. Thank you.
+          </p>
           <div className='Icons'>
           <a className='github' href="https://github.com/Premforreal" target='blank'><GitHubIcon /></a>
           <a className='portfolio' href="https://prem-react-portfolio.netlify.app/" target='blank'><TerminalIcon/></a>
